07575a5781d85a1a9c09488089692f43754efda9
YzRequiem <86611615+YzRequiem@users.noreply.github.com>
2022-01-25 15:12:00 +0100

    Add files via upload

---
+++ projet pedago/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>rhum</title>
+</head>
+<body>
+    <header>
+
+        <span> <img src="logo.png" alt="logo"> </span>
+        
+        <section>
+            <article></article>
+            <article><h1>La cave rhumanesque</h1></article>
+            <article >
+                <img class="logo" src="account.png" alt="">
+                <img class="logo" src="panier.png" alt="">
+            </article>
+</section>
+        
+        <nav>
+            <ul>
+                <li><a href="boutique">boutique</a></li>
+                <li><a href="quisommesnous">quisommesnous</a></li>
+                <li><a href="contacts">contacts</a></li>
+                <li><a href="inscription">inscription</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
